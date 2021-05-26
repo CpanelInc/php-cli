6c1590988ed1f17fa88c26a84e882b93415febd9
Daniel Muey <dan@cpanel.net>
2021-05-26 11:54:55 -0500

    ZC-8881: ensure php-cli files have correct permsissions

--- debify/debian/override_dh_auto_install.sh
+++ debify/debian/override_dh_auto_install.sh
@@ -8,8 +8,8 @@ mkdir -p $DEB_INSTALL_ROOT/usr/bin
 mkdir -p $DEB_INSTALL_ROOT/usr/local/bin
 mkdir -p $DEB_INSTALL_ROOT/var/cpanel/ea4
 
-cp -f $SOURCE0 $DEB_INSTALL_ROOT/usr/bin/php
-cp -f $SOURCE1 $DEB_INSTALL_ROOT/usr/local/bin/php
-cp -f $SOURCE2 $DEB_INSTALL_ROOT/usr/bin/lsphp
-cp -f $SOURCE3 $DEB_INSTALL_ROOT/var/cpanel/ea4/ea_php_cli.pm
+install -m 755 $SOURCE0 $DEB_INSTALL_ROOT/usr/bin/php
+install -m 755 $SOURCE1 $DEB_INSTALL_ROOT/usr/local/bin/php
+install -m 755 $SOURCE2 $DEB_INSTALL_ROOT/usr/bin/lsphp
+install -m 644 $SOURCE3 $DEB_INSTALL_ROOT/var/cpanel/ea4/ea_php_cli.pm
 
